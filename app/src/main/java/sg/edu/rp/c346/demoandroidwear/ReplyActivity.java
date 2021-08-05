bcff07f2948619873b2a586a2e205ba0171ba645
ooishujing <18016012@myrp.edu.sg>
2021-08-05 12:17:54 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/demoandroidwear/ReplyActivity.java
@@ -0,0 +1,32 @@
+package sg.edu.rp.c346.demoandroidwear;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.RemoteInput;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class ReplyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reply);
+        CharSequence reply = null;
+        Intent intent = getIntent();
+        Bundle remoteInput = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT_WATCH) {
+            remoteInput = RemoteInput.getResultsFromIntent(intent);
+        }
+        if (remoteInput != null){
+            reply = remoteInput.getCharSequence("status");
+        }
+
+        if(reply != null){
+            Toast.makeText(ReplyActivity.this, "You have indicated: " + reply,
+                    Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
